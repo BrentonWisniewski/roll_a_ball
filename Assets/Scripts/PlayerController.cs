92f349282c069f045186b135d654236642689a46
Brenton Wisniewski <bwisniewski@jvrenterprises.com>
2018-01-12 15:59:26 -0500

    Win game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public Text countText;
+	public Text winText; 
 
 	private Rigidbody rb;
 	private int count;
@@ -15,7 +16,8 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		count = 0;
-		countText.text = "Count: " + count.ToString ();
+		SetCountText ();
+		winText.text = "";
 	}
 	
     private void FixedUpdate()
@@ -33,6 +35,16 @@ public class PlayerController : MonoBehaviour {
 		{
 			other.gameObject.SetActive (false);
 			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 9)
+		{
+			winText.text = "You Win!";
 		}
 	}
 }
